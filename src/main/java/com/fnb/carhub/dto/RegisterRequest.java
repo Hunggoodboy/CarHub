307580e708a874a72ce199d6fc180b83d6c337ed
huongskc <huong2308skc@gmail.com>
2026-02-04 15:19:32 +0700

    Create DTO classes

---
+++ src/main/java/com/fnb/carhub/dto/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.fnb.carhub.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String shippingAddress; // For customer
+}
