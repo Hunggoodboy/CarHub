ca97ccc992f1980ac5eb64c0fedabf4ae1f8a080
Hunggoodboy <Hungmk2005@gmail.com>
2026-02-24 23:29:16 +0700

    add find similar car by current car

--- src/main/java/com/carhub/service/ai/VectorStoreService.java
+++ src/main/java/com/carhub/service/ai/VectorStoreService.java
@@ -1,29 +1,21 @@
 package com.carhub.service.ai;
 
+import com.carhub.dto.CarDTO;
 import com.carhub.entity.Car;
 import com.carhub.repository.CarRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.vectorstore.SearchRequest;
-import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ai.document.Document;
 
-import javax.print.Doc;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class VectorStoreService {
-//    private final File vectorFile = new File("carhub_vector_data.json");
     private final CarRepository carRepository;
     private final VectorStore vectorStore;
     //Chuyển từ Car đổi thành Document
@@ -40,12 +32,38 @@ public class VectorStoreService {
                 """, car.getName(), car.getModel(), car.getColor(), car.getDescription(), car.getPrice(), car.getDiscount(), car.getStockQuantity(), car.getManufactureYear());
         Map<String, Object> metadata = new HashMap<>();
         metadata.put("id",  car.getId());
-        metadata.put("name", car.getName());
         metadata.put("model", car.getModel());
         metadata.put("color", car.getColor());
-        metadata.put("year", car.getManufactureYear());
         metadata.put("price", car.getPrice());
+        metadata.put("discount", car.getDiscount());
+        metadata.put("stockQuantity", car.getStockQuantity());
+        metadata.put("manufactureYear", car.getManufactureYear());
         return new Document(content, metadata);
+    }
+    private CarDTO documentToDTO(Document doc) {
+        CarDTO carDTO = new CarDTO();
+        Map<String, Object> metadata = doc.getMetadata();
+        Optional.ofNullable(metadata.get("id"))
+                .ifPresent(v -> carDTO.setId(Long.parseLong(v.toString())));
+
+        Optional.ofNullable(metadata.get("model"))
+                .ifPresent(v -> carDTO.setModel(v.toString()));
+
+        Optional.ofNullable(metadata.get("color"))
+                .ifPresent(v -> carDTO.setColor(v.toString()));
+
+        Optional.ofNullable(metadata.get("price"))
+                .ifPresent(v -> carDTO.setPrice(Double.parseDouble(v.toString())));
+
+        Optional.ofNullable(metadata.get("discount"))
+                .ifPresent(v -> carDTO.setDiscount(Double.parseDouble(v.toString())));
+
+        Optional.ofNullable(metadata.get("stockQuantity"))
+                .ifPresent(v -> carDTO.setStockQuantity((Integer) v));
+
+        Optional.ofNullable(metadata.get("manufactureYear"))
+                .ifPresent(v -> carDTO.setManufactureYear((Integer) v));
+        return carDTO;
     }
     @PostConstruct
     public void loadAllCars() {
@@ -64,5 +82,22 @@ public class VectorStoreService {
         List<Document> toList = new ArrayList<>();
         toList.add(docCar);
         vectorStore.add(toList);
+    }
+    public List<CarDTO> getCarsSimilar(Long id){
+        Car car = carRepository.findById(id).orElse(null);
+        String InformationOfCar = String.format("""
+                Xe này tên là: %s,
+                Thương hiệu xe này là : %s,
+                Màu của xe là: %s,
+                Miêu tả xe: %s,
+                Xe có giá: %s,
+                Xe được giảm giá: %s,
+                Số xe còn lại là : %s,
+                Xe được bảo hành tới năm: %s
+                """, car.getName(), car.getModel(), car.getColor(), car.getDescription(), car.getPrice(), car.getDiscount(), car.getStockQuantity(), car.getManufactureYear());
+        List<Document> cars = vectorStore.similaritySearch(SearchRequest.builder().query(InformationOfCar).topK(10).build());
+        return cars.stream()
+                .map(this::documentToDTO)
+                .collect(Collectors.toList());
     }
 }
