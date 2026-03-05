837e76e57272b1ecce57e7f2914bf50e21eaad08
Hunggoodboy <Hungmk2005@gmail.com>
2026-03-05 14:34:12 +0700

    add login by google

--- src/main/java/com/carhub/config/SecurityConfig.java
+++ src/main/java/com/carhub/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.carhub.config;
 
+import com.carhub.service.Oauth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -18,6 +20,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
+    @Autowired
+    private Oauth2UserService oauth2UserService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -37,6 +41,10 @@ public class SecurityConfig {
                         .failureUrl("/login?error=true")
                         .permitAll()
                 )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .userInfoEndpoint(info -> info.userService(oauth2UserService))
+                        .defaultSuccessUrl("/", true))
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout=true")
--- src/main/java/com/carhub/entity/User.java
+++ src/main/java/com/carhub/entity/User.java
@@ -11,12 +11,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Long id;
-
+    @Column(nullable = true)
     private String username;
+    @Column(nullable = true)
     private String password; // Lưu ý: Nên mã hóa trước khi lưu
+    @Column(nullable = true)
     private String fullName;
     private String email;
+    @Column(nullable = true)
     private String phoneNumber;
+    @Column(nullable = true)
     private String address;
 
     @Enumerated(EnumType.STRING)
