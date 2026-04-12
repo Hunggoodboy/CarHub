c8e6706b536b477ba1348dbb041186312b647a53
namthang011 <thangnguyxn0105@gmail.com>
2026-04-12 09:01:21 +0700

    add revenue tab

--- src/main/java/com/carhub/controller/WebController.java
+++ src/main/java/com/carhub/controller/WebController.java
@@ -23,12 +23,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.Authentication;
 
-
 import java.io.IOException;
 import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
-
 
 @Controller
 @RequiredArgsConstructor
@@ -40,24 +38,24 @@ public class WebController {
 
     // Trang chủ
     @GetMapping({ "/", "/index" })
-public String index(Model model, Authentication authentication) {
+    public String index(Model model, Authentication authentication) {
 
-    List<CarDTO> cars = carService.getAllCars();
-    model.addAttribute("cars", cars);
+        List<CarDTO> cars = carService.getAllCars();
+        model.addAttribute("cars", cars);
 
-    if (authentication != null && authentication.isAuthenticated()
-            && !"anonymousUser".equals(authentication.getPrincipal())) {
+        if (authentication != null && authentication.isAuthenticated()
+                && !"anonymousUser".equals(authentication.getPrincipal())) {
 
-        Long userId = userService.getId(authentication);
-        UserDTO user = userService.getUserById(userId).orElse(null);
+            Long userId = userService.getId(authentication);
+            UserDTO user = userService.getUserById(userId).orElse(null);
 
-        if (user != null) {
-            model.addAttribute("fullName", user.getFullName());
+            if (user != null) {
+                model.addAttribute("fullName", user.getFullName());
+            }
         }
-    }
 
-    return "index";
-}
+        return "index";
+    }
 
     // Trang chi tiết xe
     // Trang chi tiết xe
@@ -76,36 +74,42 @@ public String index(Model model, Authentication authentication) {
     public String login() {
         return "login";
     }
-    // Trang thanh toán 
+
+    // Trang thanh toán
     @GetMapping("/payment")
     public String payment() {
         return "payment";
     }
+
     // Trang bao hành
     @GetMapping("/warranty")
-    public String warranty(){
+    public String warranty() {
         return "warranty";
     }
+
     // Trang xe đã mua
     @GetMapping("/my-cars")
     public String myCarsPage() {
         return "purchased_cars";
     }
+
     public String getMethodName(@RequestParam String param) {
         return new String();
     }
-    
+
     // Trang đăng ký
     @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("registerRequest", new RegisterRequest());
         return "register";
     }
+
     @GetMapping("/customer-view")
     public String showCustomerView(Model model) {
         model.addAttribute("car", new CarDTO());
         return "customer-view";
     }
+
     @GetMapping("/chat")
     public String chat() {
         return "chat";
@@ -113,13 +117,12 @@ public String index(Model model, Authentication authentication) {
 
     @PostMapping("/car/save")
     public String saveCar(@ModelAttribute CarDTO carDTO,
-                          @RequestParam("model") String model,
-                          @RequestParam("price") Long price,
-                          @RequestParam("manufactureYear") int manufactureYear,
-                          @RequestParam("color") String color,
-                          @RequestParam("description") String description,
-                          @RequestParam("imageFile") MultipartFile imageFile)
-    {
+            @RequestParam("model") String model,
+            @RequestParam("price") Long price,
+            @RequestParam("manufactureYear") int manufactureYear,
+            @RequestParam("color") String color,
+            @RequestParam("description") String description,
+            @RequestParam("imageFile") MultipartFile imageFile) {
         try {
             carService.saveCarService(model, price, manufactureYear, color, description, imageFile);
         } catch (IOException e) {
